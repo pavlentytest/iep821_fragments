6b25b068108caf9db4ca972d4d23240e48cd99fd
Pavel <impetus@yandex.ru>
2022-03-04 14:10:54 +0300

    Initial commit

---
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.java
@@ -0,0 +1,55 @@
+package ru.samsung.itschool.mdev.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import ru.samsung.itschool.mdev.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    public static final String KEY = "key";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater()); // присоединение
+        setContentView(binding.getRoot());
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new Fragment1());
+            }
+        });
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new Fragment2());
+            }
+        });
+    }
+
+    public void loadFragment(Fragment fr) {
+        // Менеджер переключения фрагментов
+        FragmentManager fm = getSupportFragmentManager();
+
+        Bundle bundle = new Bundle();
+        bundle.putString(KEY,"Передаем из Активности во фрагмент!");
+        fr.setArguments(bundle);
+
+        // Переключение
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.body,fr);
+        ft.addToBackStack(null);
+        ft.commit();
+
+
+    }
+}
